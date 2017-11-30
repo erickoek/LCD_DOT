e09fef77b8fd4798c2fe5c8215cfc35bfc7f46a0
erickoek <eric@koekfamily.nl>
2017-11-30 11:13:37 +0100

    added own config site

--- src/pkjs/pebble-js-app.js
+++ src/pkjs/pebble-js-app.js
@@ -1,7 +1,7 @@
 var mConfig = {};
 
 Pebble.addEventListener("ready", function(e) {
-	//console.log("91 Dub v2.0 is ready");
+	//console.log("LCD_DOT is ready");
   loadLocalData();
   returnConfigToPebble();
 });
@@ -28,10 +28,7 @@ function saveLocalData(config) {
   localStorage.setItem("invert", parseInt(config.invert)); 
   localStorage.setItem("bluetoothvibe", parseInt(config.bluetoothvibe)); 
   localStorage.setItem("hourlyvibe", parseInt(config.hourlyvibe)); 
-  localStorage.setItem("branding_mask", parseInt(config.branding_mask)); 
-	localStorage.setItem("battery_hide", parseInt(config.battery_hide)); 
-	localStorage.setItem("seconds", parseInt(config.seconds)); 
-	
+
   
   loadLocalData();
 
@@ -42,11 +39,8 @@ function loadLocalData() {
 	mConfig.invert = parseInt(localStorage.getItem("invert"));
 	mConfig.bluetoothvibe = parseInt(localStorage.getItem("bluetoothvibe"));
 	mConfig.hourlyvibe = parseInt(localStorage.getItem("hourlyvibe"));
-	mConfig.branding_mask = parseInt(localStorage.getItem("branding_mask"));
-	mConfig.battery_hide = parseInt(localStorage.getItem("battery_hide"));
-	mConfig.seconds = parseInt(localStorage.getItem("seconds"));
 	
-	mConfig.configureUrl = "http://www.mirz.com/91dubv2/index5.html";
+	mConfig.configureUrl = "https://www.koekfamily.nl/LCD_DOT/LCD_DOT.html";
 	
 	if(isNaN(mConfig.blink)) {
 		mConfig.blink = 1;
@@ -60,15 +54,7 @@ function loadLocalData() {
 	if(isNaN(mConfig.hourlyvibe)) {
 		mConfig.hourlyvibe = 0;
 	}   
-	if(isNaN(mConfig.branding_mask)) {
-		mConfig.branding_mask = 0;
-	}   
-	if(isNaN(mConfig.battery_hide)) {
-		mConfig.battery_hide = 0;
-	}   
-	if(isNaN(mConfig.seconds)) {
-		mConfig.seconds = 0;
-	}   
+	 
 
   //console.log("loadLocalData() " + JSON.stringify(mConfig));
 }
@@ -79,8 +65,5 @@ function returnConfigToPebble() {
     "invert":parseInt(mConfig.invert), 
     "bluetoothvibe":parseInt(mConfig.bluetoothvibe), 
     "hourlyvibe":parseInt(mConfig.hourlyvibe),
-    "branding_mask":parseInt(mConfig.branding_mask),
-    "battery_hide":parseInt(mConfig.battery_hide),
-    "seconds":parseInt(mConfig.seconds)
-  });    
+    });    
 }
