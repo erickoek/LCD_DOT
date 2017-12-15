d13e0dababc4ace49859f546b64b63c369b5df78
erickoek <eric@koekfamily.nl>
2017-12-15 09:45:50 +0100

    Cleanup code

--- src/c/ekk_dot.c
+++ src/c/ekk_dot.c
@@ -3,12 +3,9 @@
 
 static Window *window;
 static Layer *window_layer;
-
 static uint8_t batteryPercent;
-
 static AppSync sync;
 static uint8_t sync_buffer[128];
-
 
 #define SETTINGS_KEY 99
 
@@ -17,19 +14,14 @@ typedef struct persist {
     int Invert;
     int BluetoothVibe;
     int HourlyVibe;
-    int BrandingMask;
-    int BatteryHide;
-    int Seconds;
+
 } __attribute__((__packed__)) persist;
 
 persist settings = {
   .Blink = 0,
   .Invert = 0,
   .BluetoothVibe = 1,
-  .HourlyVibe = 0,
-  .BrandingMask = 0,
-  .BatteryHide = 0,
-  .Seconds = 0
+  .HourlyVibe = 0
 };
 
 static const PropertyAnimationImplementation layer_hidden_implementation = {
@@ -50,10 +42,7 @@ enum {
   BLINK_KEY = 0x0,
   INVERT_KEY = 0x1,
   BLUETOOTHVIBE_KEY = 0x2,
-  HOURLYVIBE_KEY = 0x3,
-  BRANDING_MASK_KEY = 0x4,
-  BATTERY_HIDE_KEY = 0x5,
-  SECONDS_KEY = 0x6
+  HOURLYVIBE_KEY = 0x3
 };
 
 enum {
@@ -62,25 +51,13 @@ enum {
   BLINK_DOUBLE_RATE = 2,
 };
 
-static GBitmap *branding_mask_image;
-static BitmapLayer *branding_mask_layer;
 
 static GBitmap *background_image;
 static BitmapLayer *background_layer;
 
-//static GBitmap *steps_image;
-//static BitmapLayer *steps_layer;
-
 static GBitmap *separator_image;
 static BitmapLayer *separator_layer;
 static PropertyAnimation *separator_animation;
-
-static GBitmap *separator_med_image;
-static BitmapLayer *separator_med_layer;
-static PropertyAnimation *separator_med_animation;
-
-//static GBitmap *meter_bar_image;
-//static BitmapLayer *meter_bar_layer;
 
 static GBitmap *bluetooth_image;
 static BitmapLayer *bluetooth_layer;
@@ -179,40 +156,6 @@ const int BIG_DIGIT_IMAGE_RESOURCE_IDS[] = {
   RESOURCE_ID_IMAGE_NUM_9
 };
 
-#define TOTAL_TIME_MED_DIGITS 4
-static GBitmap *time_med_digits_images[TOTAL_TIME_MED_DIGITS];
-static BitmapLayer *time_med_digits_layers[TOTAL_TIME_MED_DIGITS];
-
-const int MED_DIGIT_IMAGE_RESOURCE_IDS[] = {
-  RESOURCE_ID_IMAGE_MED_0,
-  RESOURCE_ID_IMAGE_MED_1,
-  RESOURCE_ID_IMAGE_MED_2,
-  RESOURCE_ID_IMAGE_MED_3,
-  RESOURCE_ID_IMAGE_MED_4,
-  RESOURCE_ID_IMAGE_MED_5,
-  RESOURCE_ID_IMAGE_MED_6,
-  RESOURCE_ID_IMAGE_MED_7,
-  RESOURCE_ID_IMAGE_MED_8,
-  RESOURCE_ID_IMAGE_MED_9
-};
-
-#define TOTAL_TIME_SM_DIGITS 2
-static GBitmap *time_sm_digits_images[TOTAL_TIME_SM_DIGITS];
-static BitmapLayer *time_sm_digits_layers[TOTAL_TIME_SM_DIGITS];
-
-const int SM_DIGIT_IMAGE_RESOURCE_IDS[] = {
-  RESOURCE_ID_IMAGE_SM_0,
-  RESOURCE_ID_IMAGE_SM_1,
-  RESOURCE_ID_IMAGE_SM_2,
-  RESOURCE_ID_IMAGE_SM_3,
-  RESOURCE_ID_IMAGE_SM_4,
-  RESOURCE_ID_IMAGE_SM_5,
-  RESOURCE_ID_IMAGE_SM_6,
-  RESOURCE_ID_IMAGE_SM_7,
-  RESOURCE_ID_IMAGE_SM_8,
-  RESOURCE_ID_IMAGE_SM_9
-};
-
 #define TOTAL_WEEK_DIGITS 3
 static GBitmap *week_images[TOTAL_WEEK_DIGITS];
 static BitmapLayer *week_layers[TOTAL_WEEK_DIGITS];
@@ -276,27 +219,18 @@ GBitmap* gbitmap_create_with_palette(uint8_t palette, uint32_t resource){
 
 void change_background() {
   gbitmap_destroy(background_image);
-  gbitmap_destroy(branding_mask_image);
   if(settings.Invert) {
     background_image = gbitmap_create_with_palette(COLOUR_DukeBlue, RESOURCE_ID_IMAGE_BACKGROUND_INVERT);
-    branding_mask_image = gbitmap_create_with_palette(COLOUR_DukeBlue, RESOURCE_ID_IMAGE_BRANDING_MASK_INVERT);
  }
  else {
     background_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_BACKGROUND);
-    branding_mask_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_BRANDING_MASK);
   }  
-
-  bitmap_layer_set_bitmap(branding_mask_layer, branding_mask_image);
-  layer_mark_dirty(bitmap_layer_get_layer(branding_mask_layer));
   
   bitmap_layer_set_bitmap(background_layer, background_image);
   layer_mark_dirty(bitmap_layer_get_layer(background_layer));
 }
 
 void change_battery_icon(bool charging) {
-  if(settings.BatteryHide) {
-    return;
-  }
   gbitmap_destroy(battery_image);
   if(charging) {
     battery_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_BATTERY_CHARGE);
@@ -316,13 +250,12 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
     case BLINK_KEY:
       settings.Blink = new_tuple->value->uint8;
       tick_timer_service_unsubscribe();
-      if(settings.Blink || settings.Seconds) {
+      if(settings.Blink) {
         tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
       }
       else {
         tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
         layer_set_hidden(bitmap_layer_get_layer(separator_layer), false);
-        layer_set_hidden(bitmap_layer_get_layer(separator_med_layer), false);
       }
       break;
     case INVERT_KEY:
@@ -335,35 +268,11 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
     case HOURLYVIBE_KEY:
       settings.HourlyVibe = new_tuple->value->uint8;
       break;
-    case BRANDING_MASK_KEY:
-      settings.BrandingMask = new_tuple->value->uint8;
-      layer_set_hidden(bitmap_layer_get_layer(branding_mask_layer), !settings.BrandingMask);
-      break;
-    case BATTERY_HIDE_KEY:
-      settings.BatteryHide = new_tuple->value->uint8;
-      layer_set_hidden(bitmap_layer_get_layer(battery_layer), settings.BatteryHide);
-      layer_set_hidden(bitmap_layer_get_layer(battery_image_layer), settings.BatteryHide);
-    
-      //APP_LOG(APP_LOG_LEVEL_DEBUG, "Battery Hide: %u", settings.BatteryHide);
-    
-      break;
-		case SECONDS_KEY:
-			settings.Seconds = new_tuple->value->uint8;
-      tick_timer_service_unsubscribe();
-      if(settings.Blink || settings.Seconds) {
-        tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
-      }
-      else {
-        tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-      }
-			if(settings.Seconds) {
-				layer_set_hidden(big_time_layer, true);
-				layer_set_hidden(med_time_layer, false);
-			}
-			else {
-				layer_set_hidden(big_time_layer, false);
-				layer_set_hidden(med_time_layer, true);		
-			}
+		 
+      tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+     	layer_set_hidden(big_time_layer, false);
+			layer_set_hidden(med_time_layer, true);		
+			
 			appStarted = false;
 			time_t now = time(NULL);
 			struct tm *tick_time = localtime(&now);  
@@ -391,10 +300,6 @@ static void set_container_image(GBitmap **bmp_image, BitmapLayer *bmp_layer, con
 }
 
 static void update_battery(BatteryChargeState charge_state) {
-  
-  if(settings.BatteryHide) {
-    return;
-  }
 
   batteryPercent = charge_state.charge_percent;
 
@@ -507,14 +412,9 @@ static void update_hours(struct tm *tick_time) {
   
   unsigned short display_hour = get_display_hour(tick_time->tm_hour);
 
-	if(settings.Seconds) {
-		set_container_image(&time_med_digits_images[0], time_med_digits_layers[0], MED_DIGIT_IMAGE_RESOURCE_IDS[display_hour/10], GPoint(12, 94));
-		set_container_image(&time_med_digits_images[1], time_med_digits_layers[1], MED_DIGIT_IMAGE_RESOURCE_IDS[display_hour%10], GPoint(35, 94));  
-	}
-	else {
+
 		set_container_image(&time_digits_images[0], time_digits_layers[0], BIG_DIGIT_IMAGE_RESOURCE_IDS[display_hour/10], GPoint(3, 117));
 		set_container_image(&time_digits_images[1], time_digits_layers[1], BIG_DIGIT_IMAGE_RESOURCE_IDS[display_hour%10], GPoint(31, 117));
-	}
     
   if (!clock_is_24h_style()) {
     if (tick_time->tm_hour >= 12) {
@@ -525,69 +425,34 @@ static void update_hours(struct tm *tick_time) {
       layer_set_hidden(bitmap_layer_get_layer(time_format_layer), true);
     }
     if (display_hour/10 == 0) {
-			if(settings.Seconds) {
-				layer_set_hidden(bitmap_layer_get_layer(time_med_digits_layers[0]), true);
-			}
-			else {
+			
 				layer_set_hidden(bitmap_layer_get_layer(time_digits_layers[0]), true);
-			}
     }
     else {
-			if(settings.Seconds) {
-				layer_set_hidden(bitmap_layer_get_layer(time_med_digits_layers[0]), false);
-			}
-			else {
+			
 				layer_set_hidden(bitmap_layer_get_layer(time_digits_layers[0]), false);
 			}
-    }
-
   }
 }
+
 static void update_minutes(struct tm *tick_time) {
-	if(settings.Seconds) {
-		set_container_image(&time_med_digits_images[2], time_med_digits_layers[2], MED_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min/10], GPoint(65, 88));
-		set_container_image(&time_med_digits_images[3], time_med_digits_layers[3], MED_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min%10], GPoint(87, 88));
-	}
-	else {
 		set_container_image(&time_digits_images[2], time_digits_layers[2], BIG_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min/10], GPoint(87, 117));
 		set_container_image(&time_digits_images[3], time_digits_layers[3], BIG_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_min%10], GPoint(115, 117));
-	}
-
 }
+  
 static void update_seconds(struct tm *tick_time) {
 
   if(settings.Blink == BLINK_ON) {
     animation_unschedule_all();
-               if(settings.Seconds) {
-                       layer_set_hidden(bitmap_layer_get_layer(separator_med_layer), tick_time->tm_sec%2);
-               }
-               else {
-                       layer_set_hidden(bitmap_layer_get_layer(separator_layer), tick_time->tm_sec%2);
-               }
+    layer_set_hidden(bitmap_layer_get_layer(separator_layer), tick_time->tm_sec%2);
   }
-  else if(settings.Blink == BLINK_DOUBLE_RATE) {
-		if(settings.Seconds) {
-			layer_set_hidden(bitmap_layer_get_layer(separator_med_layer), false);
-			animation_schedule((Animation *)separator_med_animation);
-		}
-		else {
-			layer_set_hidden(bitmap_layer_get_layer(separator_layer), false);
-			animation_schedule((Animation *)separator_animation);
-		}
-  }
+ 
   else if (settings.Blink == BLINK_OFF) {
 			animation_unschedule_all();
-			if(settings.Seconds) {
-				layer_set_hidden(bitmap_layer_get_layer(separator_med_layer), false);
-			}
-			else {
+  }	
+    else {
 				layer_set_hidden(bitmap_layer_get_layer(separator_layer), false);
 			}	
-  }
-	if(settings.Seconds) {
-		set_container_image(&time_sm_digits_images[0], time_sm_digits_layers[0], SM_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_sec/10], GPoint(109, 104));
-		set_container_image(&time_sm_digits_images[1], time_sm_digits_layers[1], SM_DIGIT_IMAGE_RESOURCE_IDS[tick_time->tm_sec%10], GPoint(121, 104));
-	}
 }
 
 static void health_handler(HealthEventType event, void *context) {
@@ -626,11 +491,6 @@ static void savePersistentSettings() {
 static void init(void) {
   memset(&time_digits_layers, 0, sizeof(time_digits_layers));
   memset(&time_digits_images, 0, sizeof(time_digits_images));
-  memset(&time_med_digits_layers, 0, sizeof(time_med_digits_layers));
-  memset(&time_med_digits_images, 0, sizeof(time_med_digits_images));
-  memset(&time_sm_digits_layers, 0, sizeof(time_sm_digits_layers));
-  memset(&time_sm_digits_images, 0, sizeof(time_sm_digits_images));
-
   memset(&date_digits_layers, 0, sizeof(date_digits_layers));
   memset(&date_digits_images, 0, sizeof(date_digits_images));
   memset(&week_layers, 0, sizeof(week_layers));
@@ -665,16 +525,6 @@ static void init(void) {
   med_time_layer = layer_create(layer_get_frame(window_layer));
   layer_add_child(window_layer, med_time_layer);
   
-  //steps_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_TESTWEER);
-  //GRect frame = (GRect) {
-  // .origin = { .x = 3, .y = 20},
-  // .size = gbitmap_get_bounds(steps_image).size
-  //};
-  //steps_layer = bitmap_layer_create(frame);
-
-  //bitmap_layer_set_bitmap(steps_layer, steps_image);
-  //layer_add_child(window_layer, bitmap_layer_get_layer(steps_layer));  
-  
   separator_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_SEPARATOR);
   GRect frame_steps = (GRect) {
     .origin = { .x = 59, .y = 117},
@@ -685,26 +535,6 @@ static void init(void) {
   
   bitmap_layer_set_bitmap(separator_layer, separator_image);
   layer_add_child(big_time_layer, bitmap_layer_get_layer(separator_layer));   
-  
-  separator_med_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_MED_SEPARATOR);
-  GRect frame_med = (GRect) {
-    .origin = { .x = 57, .y = 93 },
-    .size = gbitmap_get_bounds(separator_med_image).size
-  };
-  separator_med_layer = bitmap_layer_create(frame_med);
-  bitmap_layer_set_bitmap(separator_med_layer, separator_med_image);
-  layer_add_child(med_time_layer, bitmap_layer_get_layer(separator_med_layer));  
-  layer_set_hidden(med_time_layer, true);
-
-  //meter_bar_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_METER_BAR);
-  //GRect frame2 = (GRect) {
-  //  .origin = { .x = 17, .y = 5 },
-  //  .size = gbitmap_get_bounds(meter_bar_image).size
-  //};
-  //meter_bar_layer = bitmap_layer_create(frame2);
-  //bitmap_layer_set_bitmap(meter_bar_layer, meter_bar_image);
-  //layer_add_child(window_layer, bitmap_layer_get_layer(meter_bar_layer));  
-
  
   bluetooth_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_BLUETOOTH);
   GRect frame3 = (GRect) {
@@ -753,14 +583,6 @@ static void init(void) {
     time_digits_layers[i] = bitmap_layer_create(dummy_frame);
     layer_add_child(big_time_layer, bitmap_layer_get_layer(time_digits_layers[i]));
   }
-  for (int i = 0; i < TOTAL_TIME_MED_DIGITS; ++i) {
-    time_med_digits_layers[i] = bitmap_layer_create(dummy_frame);
-    layer_add_child(med_time_layer, bitmap_layer_get_layer(time_med_digits_layers[i]));
-  }
-  for (int i = 0; i < TOTAL_TIME_SM_DIGITS; ++i) {
-    time_sm_digits_layers[i] = bitmap_layer_create(dummy_frame);
-    layer_add_child(med_time_layer, bitmap_layer_get_layer(time_sm_digits_layers[i]));
-  }
   for (int i = 0; i < TOTAL_DATE_DIGITS; ++i) {
     date_digits_layers[i] = bitmap_layer_create(dummy_frame);
     layer_add_child(window_layer, bitmap_layer_get_layer(date_digits_layers[i]));
@@ -777,17 +599,6 @@ static void init(void) {
     battery_percent_layers[i] = bitmap_layer_create(dummy_frame);
     layer_add_child(window_layer, bitmap_layer_get_layer(battery_percent_layers[i]));
   }
-    
-  //mask the pebble branding
-  GRect framemask = (GRect) {
-    .origin = { .x = 0, .y = 0 },
-    .size = { .w = 144, .h = 19 }
-  };
-  branding_mask_layer = bitmap_layer_create(framemask);
-  layer_add_child(window_layer, bitmap_layer_get_layer(branding_mask_layer));
-  branding_mask_image = gbitmap_create_with_palette(COLOUR_USER, RESOURCE_ID_IMAGE_BRANDING_MASK);
-  bitmap_layer_set_bitmap(branding_mask_layer, branding_mask_image);
-  layer_set_hidden(bitmap_layer_get_layer(branding_mask_layer), !settings.BrandingMask);
   
   toggle_bluetooth_icon(bluetooth_connection_service_peek());
   update_battery(battery_state_service_peek());
@@ -796,10 +607,7 @@ static void init(void) {
     TupletInteger(BLINK_KEY, settings.Blink),
     TupletInteger(INVERT_KEY, settings.Invert),
     TupletInteger(BLUETOOTHVIBE_KEY, settings.BluetoothVibe),
-    TupletInteger(HOURLYVIBE_KEY, settings.HourlyVibe),
-    TupletInteger(BRANDING_MASK_KEY, settings.BrandingMask),
-    TupletInteger(BATTERY_HIDE_KEY, settings.BatteryHide),
-    TupletInteger(SECONDS_KEY, settings.Seconds)
+    TupletInteger(HOURLYVIBE_KEY, settings.HourlyVibe)
   };
   
   app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
@@ -814,11 +622,6 @@ static void init(void) {
   animation_set_delay((Animation *) separator_animation, 500);
   animation_set_curve((Animation *) separator_animation, AnimationCurveLinear);
 
-  separator_med_animation = property_animation_create(&layer_hidden_implementation, bitmap_layer_get_layer(separator_med_layer), &_false, &_true);
-  animation_set_duration((Animation *) separator_med_animation, 1);
-  animation_set_delay((Animation *) separator_med_animation, 500);
-  animation_set_curve((Animation *) separator_med_animation, AnimationCurveLinear);
-
   // Avoids a blank screen on watch start.
   time_t now = time(NULL);
   struct tm *tick_time = localtime(&now);  
@@ -826,13 +629,8 @@ static void init(void) {
 
   appStarted = true;
 
-	if(settings.Seconds) {
-		tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
-	}
-	else {
-  	tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-	}
-
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+	
   bluetooth_connection_service_subscribe(bluetooth_connection_callback);
   battery_state_service_subscribe(&update_battery);
   
@@ -860,9 +658,6 @@ static void deinit(void) {
 
   animation_unschedule_all();
 
-  property_animation_destroy(separator_med_animation);
-  separator_med_animation = NULL;
-
   property_animation_destroy(separator_animation);
   separator_animation = NULL;
 
@@ -871,30 +666,10 @@ static void deinit(void) {
   gbitmap_destroy(background_image);
   background_image = NULL;
   
-  layer_remove_from_parent(bitmap_layer_get_layer(branding_mask_layer));
-  bitmap_layer_destroy(branding_mask_layer);
-  gbitmap_destroy(branding_mask_image);
-  branding_mask_image = NULL;
-
-  //layer_remove_from_parent(bitmap_layer_get_layer(steps_layer));
-  //bitmap_layer_destroy(steps_layer);
-  //gbitmap_destroy(steps_image);
-  //steps_image = NULL;
-  
   layer_remove_from_parent(bitmap_layer_get_layer(separator_layer));
   bitmap_layer_destroy(separator_layer);
   gbitmap_destroy(separator_image);
   separator_image = NULL;
-
-  layer_remove_from_parent(bitmap_layer_get_layer(separator_med_layer));
-  bitmap_layer_destroy(separator_med_layer);
-  gbitmap_destroy(separator_med_image);
-  separator_med_image = NULL; 
-  
-  //layer_remove_from_parent(bitmap_layer_get_layer(meter_bar_layer));
-  //bitmap_layer_destroy(meter_bar_layer);
-  //gbitmap_destroy(meter_bar_image);
-  //background_image = NULL;
   
   layer_remove_from_parent(bitmap_layer_get_layer(bluetooth_layer));
   bitmap_layer_destroy(bluetooth_layer);
@@ -954,22 +729,6 @@ static void deinit(void) {
     time_digits_images[i] = NULL;
     bitmap_layer_destroy(time_digits_layers[i]);
     time_digits_layers[i] = NULL;
-  }
-
-  for (int i = 0; i < TOTAL_TIME_MED_DIGITS; i++) {
-    layer_remove_from_parent(bitmap_layer_get_layer(time_med_digits_layers[i]));
-    gbitmap_destroy(time_med_digits_images[i]);
-    time_med_digits_images[i] = NULL;
-    bitmap_layer_destroy(time_med_digits_layers[i]);
-    time_med_digits_layers[i] = NULL;
-  }
-
-  for (int i = 0; i < TOTAL_TIME_SM_DIGITS; i++) {
-    layer_remove_from_parent(bitmap_layer_get_layer(time_sm_digits_layers[i]));
-    gbitmap_destroy(time_sm_digits_images[i]);
-    time_sm_digits_images[i] = NULL;
-    bitmap_layer_destroy(time_sm_digits_layers[i]);
-    time_sm_digits_layers[i] = NULL;
   }
 
   for (int i = 0; i < TOTAL_BATTERY_PERCENT_DIGITS; i++) {
